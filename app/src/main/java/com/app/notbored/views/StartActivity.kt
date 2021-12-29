8491ca978316dff14b46f5bd24a24fd55e154790
Alejandra Casuscelli <alejandra.casuscelli@mercadolibre.com>
2021-12-29 13:04:30 -0300

    start view

---
+++ app/src/main/java/com/app/notbored/views/StartActivity.kt
@@ -0,0 +1,38 @@
+package com.app.notbored.views
+
+import android.content.res.Configuration
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.core.widget.addTextChangedListener
+import com.app.notbored.R
+import com.app.notbored.databinding.ActivityStartBinding
+
+class StartActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityStartBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        configurateView()
+    }
+
+    private fun configurateView() {
+        with(binding) {
+            editTextParticipants.addTextChangedListener {
+                startButton.isEnabled = !it.toString().contains("." ) || !it.toString().contains(",")
+            }
+
+            startButton.setOnClickListener {
+
+            }
+
+            textViewTerms.setOnClickListener {
+                
+            }
+        }
+    }
+}
