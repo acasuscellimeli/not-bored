1ddcf1031e30b186466013e902564d8924c4d9be
Alejandra Casuscelli <alejandra.casuscelli@mercadolibre.com>
2022-01-03 12:10:17 -0300

    Adding images and style resources

--- app/src/main/java/com/app/notbored/constants/Constants.kt
+++ app/src/main/java/com/app/notbored/constants/Constants.kt
@@ -6,7 +6,7 @@ object Constants {
     val ACTIVITIES = listOf("education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork")
     const val PARTICIPANTS = "participants_key"
     const val PARTICIPANT_DEFAULT_VALUE = 1
-    const val BASE_URL = "http://www.boredapi.com/api/"
+    const val BASE_URL = "https://www.boredapi.com/api/"
     const val SHARED_PREF = "sharedpref"
     const val INTENT_DETAILS = "activityResponse"
     const val INTENT_RANDOM = "randomActivity"
